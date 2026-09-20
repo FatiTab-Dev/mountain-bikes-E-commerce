5967aa11b54f3de2f5fc4541bc34ba4b59b443c9
Fatima <tabtfatima8@gmail.com>
2026-09-20 22:03:30 +0100

    add link frontEnd

--- Back-end/server.js
+++ Back-end/server.js
@@ -15,16 +15,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      'mountain-bikes-e-commerce-53lu-1u47troer-fatitab-devs-projects.vercel.app',
+  })
+);
 app.use(express.json());
 
-// Routes
-app.use('/api/products', productRoutes);
-app.use('/api/orders', orderRoutes);
 app.use((req, res, next) => {
   console.log(`[REQUEST] ${req.method} ${req.url}`);
   next();
 });
+
+// Routes
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+
 app.use('/api/auth', authRoutes);
 app.use('/img', express.static('public/img'));
 
